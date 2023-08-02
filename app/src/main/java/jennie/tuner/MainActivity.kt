f87c68e03c32ec263b50d99124f12e2cf2da8753
Jennifer TER <jennifer.ter@epita.com>
2023-08-02 21:38:02 +0200

    Get the component by id

--- app/src/main/java/jennie/tuner/MainActivity.kt
+++ app/src/main/java/jennie/tuner/MainActivity.kt
@@ -2,10 +2,26 @@ package jennie.tuner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var startRecordingButton: Button
+    private lateinit var stopRecordingButton: Button
+    private lateinit var startPlayingButton: Button
+    private lateinit var stopPlayingButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startRecordingButton = findViewById(R.id.record_button)
+        stopRecordingButton = findViewById(R.id.stop_recording_button)
+        startPlayingButton = findViewById(R.id.play_button)
+        stopPlayingButton = findViewById(R.id.stop_button)
+
+
+
+
     }
 }
