d10ce54233b859f224e880c4464444c49658b661
Jennifer TER <jennifer.ter@epita.com>
2023-08-05 20:10:07 +0200

    Can record sound and ask permission

--- app/src/main/java/jennie/tuner/MainActivity.kt
+++ app/src/main/java/jennie/tuner/MainActivity.kt
@@ -1,8 +1,10 @@
 package jennie.tuner
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.media.MediaPlayer
+import android.media.AudioFormat
+import android.media.AudioRecord
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.widget.Button
@@ -17,15 +19,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var stopRecordingButton: Button
     private lateinit var freq_value: TextView
 
-    private var mediaRecorder: MediaRecorder? = null
-    private var mediaPlayer: MediaPlayer? = null
+    private var audioRecord: AudioRecord? = null
+    private val audioSource = MediaRecorder.AudioSource.MIC
+    private val channelConfig = AudioFormat.CHANNEL_IN_MONO
+    private val audioFormat = AudioFormat.ENCODING_PCM_16BIT
+
     private val samplingRate = 44100 // Default sampling rate for audio
 
+    private val bufferSize: Int = AudioRecord.getMinBufferSize(
+        samplingRate,
+        channelConfig,
+        audioFormat
+    )
+
+
     private fun isMicrophonePermissionGranted(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.RECORD_AUDIO
-        ) == PackageManager.PERMISSION_GRANTED
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,11 +49,45 @@ class MainActivity : AppCompatActivity() {
 
         if (!isMicrophonePermissionGranted()) {
             ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.RECORD_AUDIO), RECORD_AUDIO_PERMISSION_CODE
+                this, arrayOf(Manifest.permission.RECORD_AUDIO),
+                RECORD_AUDIO_PERMISSION_CODE // Add the constant here
             )
         }
 
+        startRecordingButton.setOnClickListener() {
+            startRecording()
+        }
+
+        stopRecordingButton.setOnClickListener() {
+            stopRecording()
+        }
+
     }
+
+
+    @SuppressLint("MissingPermission")
+    private fun startRecording() {
+        audioRecord = AudioRecord(
+            audioSource,
+            samplingRate,
+            channelConfig,
+            audioFormat,
+            bufferSize
+        )
+
+        audioRecord?.startRecording()
+
+        //analyzeAudio()
+        freq_value.text = "Hello World"
+    }
+
+    private fun stopRecording() {
+        audioRecord?.stop()
+        audioRecord?.release()
+        audioRecord = null
+    }
+
+
 
     companion object {
         const val RECORD_AUDIO_PERMISSION_CODE = 1
